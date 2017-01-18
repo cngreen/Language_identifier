4b8ee1e3d8b974f4de3b67a1b9211bc60bc1f335
Kari Green <Kari@Karis-MacBook-Pro.local>
2017-01-18 13:18:48 -0500

    better find

--- test.py
+++ test.py
@@ -1,21 +1,30 @@
 import re
 
 def indentifyDates(input):
+	#finds numerical dates with years of 4 or two digits separated by - / . or ,
 	matches = []
-	#finds dates of format 01.01.17 01/01/17 01.01.2017 01/01/2017 01-01-17 01-01-2017 
-	# 1.1.17 1/1/17 1.1.2017 1/1/2017 1-1-17 1-1-2017
-	# 1, 17 01, 17 1, 2017 01, 2017
-
 
 	# XXXX-X/X-X/X
-	match = re.search(r'\d{4}[-/.]\d{1,2}[-/.]\d{1,2}', input)
+	print(input)
+	match = re.findall(r'\d{4}[-/.,]\d{1,2}[-/.,]\d{1,2}', input)
 	if match != None:
-		matches.append(match.group())
+		for m in match:
+			matches.append(m)
+		input = re.sub(r'\d{4}[-/.,]\d{1,2}[-/.,]\d{1,2}', '', input)
+		
 	# X/X-X/X-XXXX
-	match = re.search(r'\d{1,2}-\d{1,2}-\d{4}', input)
+	match = re.search(r'\d{1,2}[-/.,]\d{1,2}[-/.,]\d{4}', input)
 	if match != None:
 		matches.append(match.group())
 
+	#XX-X/X-X/X
+	match = re.search(r'\d{2}[-/.,]\d{1,2}[-/.,]\d{1,2}', input)
+	if match != None:
+		matches.append(match.group())
+	#X/X-X/X-XX
+	match = re.search(r'\d{1,2}[-/.,]\d{1,2}[-/.,]\d{2}', input)
+	if match != None:
+		matches.append(match.group())
 
 	print matches
 	return
@@ -23,7 +32,7 @@ def indentifyDates(input):
 
 
 def main():
-	test = 'here is a 1031/1/20 string with 1.1.16 a of 1, 2017 dates may 17, 1993 3.3.16 1-1-10 string string'
+	test = 'here is a 1031/1/20 string with 1.1.16 a of 1, 2017 dates 1015/12/1 may 17, 1993 3.3.16 1-1-10 string string'
 
 	indentifyDates(test)
 
